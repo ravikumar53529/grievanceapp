7bfb12a95f26f4c42ef137817b989d4896e7b852
ravi53529 <galinki.ravi333@gmail.com>
2023-05-22 10:35:10 +0530

    added basic ui for grievance dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid
+} from "ng-apexcharts";
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+};
+interface City {
+    name: string;
+    code: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +31,131 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grievanceapp';
+  public data: any;
+
+   public  options: any;
+   public  data1: any;
+
+  public options1: any;
+  public chartOptions: any;
+   public  cities: City[]=[];
+   public selectedCity!: City;
+   public form!:FormGroup
+  constructor(private fb:FormBuilder) {
+    
+  }
+  
+  ngOnInit() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+      
+      this.data = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {
+                  type: 'line',
+                  label: 'New Tickets',
+                  borderColor: documentStyle.getPropertyValue('--green-500'),
+                  borderWidth: 2,
+                  fill: false,
+                  tension: 0.4,
+                  data: [21, 84, 24, 75, 37, 65, 34]
+              },
+              {
+                  type: 'bar',
+                  label: 'Closed Tickets',
+                  backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+                  data: [18, 84, 23, 75, 30, 61, 30],
+                  borderColor: 'white',
+                  borderWidth: 2
+              },
+              {
+                  type: 'bar',
+                  label: 'Backlog Tickets',
+                  backgroundColor: documentStyle.getPropertyValue('--orange-500'),
+                  data: [41, 52, 24, 74, 23, 21, 32]
+              },
+              
+          ]
+      };
+      
+      this.options = {
+          maintainAspectRatio: false,
+          aspectRatio: 0.6,
+          plugins: {
+              legend: {
+                  labels: {
+                      color: textColor
+                  }
+              }
+          },
+          scales: {
+              x: {
+                  ticks: {
+                      color: textColorSecondary
+                  },
+                  grid: {
+                      color: surfaceBorder
+                  }
+              },
+              y: {
+                  ticks: {
+                      color: textColorSecondary
+                  },
+                  grid: {
+                      color: surfaceBorder
+                  }
+              },
+              
+          }
+      };
+      this.getDoughnut()
+      this.onDropDown()
+      this.onForm()
+  }
+
+  public getDoughnut():void{
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    this.data1 = {
+        labels: ['Web', 'Email', 'Chat','Phone'],
+        datasets: [
+            {
+                data: [300, 50, 100,20],
+                backgroundColor: [documentStyle.getPropertyValue('--red-500'), documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--green-500')],
+                hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+            }
+        ]
+    };
+
+
+    this.options1 = {
+        cutout: '60%',
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        }
+    };
+}
+  
+ public onDropDown():void{
+    this.cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+ }
+ public onForm():void{
+    this.form=this.fb.group({
+        city:new FormControl()
+    })
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ButtonModule } from 'primeng/button';
+import { ChartModule } from 'primeng/chart';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { ProgressBarModule} from 'primeng/progressbar';
+import { DropdownModule } from 'primeng/dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    ChartModule,
+    NgApexchartsModule,
+    ProgressBarModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
